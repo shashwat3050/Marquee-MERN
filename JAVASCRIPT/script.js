481e7661c8917a16062c6cd56e0fef416c4f359e
shashwat3050 <shashwat3050@gmail.com>
2026-06-29 16:39:55 +0530

    Started Javascript

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,14 @@
+//alert('Hello World');
+
+console.log("text area");
+console.warn("text area");
+console.error("text area");
+console.groupEnd();
+
+console.table({name:'DoLand Trump', age:64, Type:'Pedophilic'});
+
+function greet(){
+    console.warn("diwakar sapola");
+}
+
+greet();
