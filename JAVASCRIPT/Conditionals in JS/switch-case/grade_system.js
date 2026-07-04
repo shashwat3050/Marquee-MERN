3dc96443cb4b10b1db4335f29568b1bc288ff04f
shashwat3050 <shashwat3050@gmail.com>
2026-07-04 10:44:44 +0530

    conditional statements in js

---
+++ JAVASCRIPT/Conditionals in JS/switch-case/grade_system.js
@@ -0,0 +1,20 @@
+function getGradeByScore(score) {
+    if(score<0 || score>100 || typeof score !=='number') {
+        return "Invalid Score";
+    }
+
+    switch(true) {
+        case score >= 90:
+            return "A";
+        case score >= 80:
+            return "B";
+        case score >= 70:
+            return "C";
+        case score >= 60:
+            return "D";
+        default:
+            return "F";
+    }
+}
+
+console.log(getGradeByScore(95));
