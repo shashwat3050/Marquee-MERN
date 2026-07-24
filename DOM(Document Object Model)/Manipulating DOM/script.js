8c68bd199e479a684c80db2f5908350f526fa49d
shashwat3050 <shashwat3050@gmail.com>
2026-07-24 13:54:14 +0530

    DOM Manipulation practice

--- DOM(Document Object Model)/Manipulating DOM/script.js
+++ DOM(Document Object Model)/Manipulating DOM/script.js
@@ -27,14 +27,54 @@ fruit_name.forEach((fruit) =>{
 
 //3. before begin  -----------------------------
 
+//----- through JS -----
 // const before_begin = document.getElementById('parent');
 // before_begin.insertAdjacentHTML('beforebegin', '<p>this is added </strong> before begin</strong></p>');
 
 //----- through DOM Manipulation -----
-const e = document.getElementById('parent');
+const add_ele = document.getElementById('parent');
 const added_header = document.createElement('h2');
 added_header.textContent = 'this is added as "before begin"';
-e.before(added_header);  //to add at last just change 'before' to 'after'
+add_ele.before(added_header);  //to add at last just change 'before' to 'after'
 
 // also, after begin = prepend
 //       before end = append
+
+
+
+//4. Parent-child relation ------------------------------
+
+//-----remove child -----
+let remove_list = document.getElementById('parent');
+let lastChild = remove_list.lastElementChild; //this will remove the last list from the original
+                                              // if we add ".previousElementSibling" then this will remove the last second element
+remove_list.removeChild(lastChild);
+
+//----- clone child -----
+let clone_child = document.getElementById('parent');
+let clone_item = list.lastElementChild;
+let cloned = clone_item.cloneNode(true);
+clone_child.append(cloned);
+
+
+
+//5. Insert before -------------------------------
+
+let insert_item = document.getElementById('parent');
+let p = input_item.parentNode;
+let new_item = document.createElement('li');
+new_item.textContent = 'Done using "Insert Before!"';
+
+insert_item.insertBefore(new_item, first_item);
+
+
+
+//6. attribute --------------------------------
+
+let input_item = document.getElementById('input_text');
+
+let get_id = input_item.getAttribute('id');
+let get_val = input_item.getAttribute('value');
+
+console.log(get_id,",", get_val);
+
